14d610d8dfacbf69193e39be98a99ab9cebc58c2
Peter <Peter@PeterH-pc>
2018-02-07 11:10:08 +0100

    First Actual commit to first git repository

--- ConcurrentProgrammingExercises/src/concurrentprogrammingexercises/ConcurrentProgrammingExercises.java
+++ ConcurrentProgrammingExercises/src/concurrentprogrammingexercises/ConcurrentProgrammingExercises.java
@@ -5,16 +5,42 @@
  */
 package concurrentprogrammingexercises;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Peter
  */
 public class ConcurrentProgrammingExercises {
 
+    private class Pair<K,T>{
+        private K obj1;
+        private T obj2;
+        
+        private void setK ( K obj1 ){
+            this.obj1 = obj1;
+        }
+        
+        private K getK (){
+            return this.obj1;
+        }
+        
+        private void setT ( T obj2 ){
+            this.obj2 = obj2;
+        }
+        
+        private T getT (){
+            return this.obj2;
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        List<Pair<String,Integer>> l;
+        l = new LinkedList<>();
         
     }
     
